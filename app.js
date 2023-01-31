cfef51d50cdd3dda703b9f3ce184283cb9ec3c9b
Jasa95 <jaco3020@stud.kea.dk>
2023-01-31 12:04:10 +0100

    Lucas er en luder

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-document.body.innerHTML += "Hello JavaScript!";
+//document.body.innerHTML += "Hello JavaScript!";
 // alert("Hello Dat.js");
-console.log("Hello Dat.js")
-console.log("Hi Student!")
-console.log("This is my first Javascript!")
+//console.log("Hello Dat.js")
+//console.log("Hi Student!")
+//console.log("This is my first Javascript!")
+
+// Du kan se hvordan du styler hjemmesiden på mdn web docs
+
+function sayHallo(){
+document.querySelector("h3").textContent = "Hej " + document.querySelector("input").value;
+}
+document.querySelector("button").addEventListener("click", sayHallo);
